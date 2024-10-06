caef30483e8aa8c05020c5d4a20551dfa5d96471
gowthamthotakura <gowthamthotakura@gmail.com>
2024-10-06 03:09:45 -0400

    Adding temporary changes to Github

---
+++ src/controllers/userController.js
@@ -0,0 +1,68 @@
+const User = require('../models/userModels.js/userModel');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+// User registration
+exports.registerUser = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    // Check if the username already exists
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(409).json({ message: 'Username already exists' });
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create a new user
+    const newUser = new User({ username, password: hashedPassword });
+    await newUser.save();
+
+    return res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+// User login
+exports.loginUser = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    // Check if the username exists
+    const existingUser = await User.findOne({ username });
+    if (!existingUser) {
+      return res.status(401).json({ message: 'Invalid username or password' });
+    }
+
+    // Check if the password is correct
+    const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
+    if (!isPasswordCorrect) {
+      return res.status(401).json({ message: 'Invalid username or password' });
+    }
+
+    // Generate a JSON Web Token (JWT)
+    const token = jwt.sign({ username: existingUser.username }, '76893a17c8fcf41e4510184ee0a5e7549514fb38a4a8ac1ebf8ffc99da512a90', { expiresIn: '1h' });
+
+    return res.status(200).json({ token });
+  } catch (error) {
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+// User profile management
+exports.updateUserProfile = async (req, res) => {
+  try {
+    const { username } = req.params;
+    const { newUsername } = req.body;
+
+    // Update the user's username
+    await User.updateOne({ username }, { username: newUsername });
+
+    return res.status(200).json({ message: 'User profile updated successfully' });
+  } catch (error) {
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+};
